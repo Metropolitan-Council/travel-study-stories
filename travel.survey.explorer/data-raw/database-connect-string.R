304dc4d7f72418a2629c14277615a1aa71fcde67
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-03-24 10:35:59 -0500

    new _data_compile process
    from vehicle-use-analysis

---
+++ travel.survey.explorer/data-raw/database-connect-string.R
@@ -0,0 +1,1 @@
+connect_string <- "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = fth-exa-scan.mc.local  )(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME =  com4te.mc.local)))"
