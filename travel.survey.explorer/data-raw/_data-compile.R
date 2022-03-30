272f53e37f10935ef3a612c684e7b639fd5fa326
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-03-30 15:33:50 -0500

    refreshed dictionary

--- travel.survey.explorer/data-raw/_data-compile.R
+++ travel.survey.explorer/data-raw/_data-compile.R
@@ -1,6 +1,9 @@
 # Get TBI survey data from database ---------
 source("data-raw/get-survey-data.R")
 
+# Work on the dictionary ------------------
+source("data-raw/create-dictionary.R")
+
 # Trim columns down for manageability ----------
 source("data-raw/slim-survey-data-columns.R")
 
@@ -56,70 +59,6 @@ tbi_tables <- list(
 )
 
 usethis::use_data(tbi_tables,
-                  overwrite = TRUE,
-                  compress = "xz",
-                  internal = FALSE
-)
-
-
-tbi_dict <- dictionary %>%
-  filter(category %in% c(
-    "Demographics",
-    "Attitudes toward autonomous vehicles",
-    "Shared mobility",
-    "Commute",
-    "Trips",
-    "Days without travel",
-    "Delivery & online shopping",
-    "Vehicle"
-  )) %>%
-  # we'll probably want to play with the ORDERING of this case-when command
-  # to assign variables to tables when they appear in multiple tables.
-  mutate(which_table = case_when(
-    variable %in% names(per) ~ "per",
-    variable %in% names(hh) ~ "hh",
-    variable %in% names(trip) ~ "trip",
-    variable %in% names(day) ~ "day",
-    variable %in% names(veh) ~ "veh"
-  )) %>%
-  # find the weighting field for each table:
-  mutate(wt_field = case_when(
-    which_table == "per" ~ "person_weight",
-    which_table == "hh" ~ "hh_weight",
-    which_table == "trip" ~ "trip_weight",
-    which_table == "day" ~ "day_weight",
-    which_table == "veh" ~ "hh_weight"
-  ))
-
-# # Appending missing variables to tbi_dict
-# missing_vars <-
-# lapply(tbi_tables, function(x) setdiff(names(x), dictionary$variable) %>% as.data.frame()) %>%
-#   rbindlist(idcol = "which_table")
-#
-# names(missing_vars) <- c("which_table", "variable")
-#
-# # trim out ID's and weights
-# missing_vars <-
-# missing_vars %>%
-#   filter(!grepl("_id", variable)) %>%
-#   filter(!grepl("_weight", variable)) %>%
-#   filter(!grepl("_num", variable)) %>%
-#   filter(!grepl("_date", variable)) %>%
-#   mutate(wt_field = case_when(
-#     which_table == "per" ~ "person_weight",
-#     which_table == "hh" ~ "hh_weight",
-#     which_table == "trip" ~ "trip_weight",
-#     which_table == "day" ~ "day_weight",
-#     which_table == "veh" ~ "hh_weight"
-#   ))
-#
-# write.csv(full_join(missing_vars, dictionary), "data-raw/full_dictionary.csv")
-
-
-# some work by hand occurred:
-tbi_dict <- read.csv('data-raw/full_dictionary.csv')
-
-usethis::use_data(tbi_dict,
                   overwrite = TRUE,
                   compress = "xz",
                   internal = FALSE
