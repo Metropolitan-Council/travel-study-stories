d5d09e0c9f964e4cdd5a91f39fafebc8d5c9db8a
Liz Roten <liz.roten@metc.state.mn.us>
2022-04-05 12:25:13 -0500

    create utility module for generating table data

---
+++ travel.survey.explorer/R/mod_input_utils.R
@@ -0,0 +1,45 @@
+#' input_utils UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_input_utils_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+#' input_utils Server Functions
+#'
+#' @noRd
+mod_input_utils_server <- function(id, user_inputs){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    # browser()
+
+
+    vals <- reactiveValues()
+
+    table_return <- reactive({
+      create_one_way_table(user_inputs$variable)
+      })
+
+    observe({
+      vals$table_data <- table_return()
+    })
+
+
+    return(vals)
+  })
+}
+
+## To be copied in the UI
+# mod_input_utils_ui("input_utils_ui_1")
+
+## To be copied in the server
+# mod_input_utils_server("input_utils_ui_1")
