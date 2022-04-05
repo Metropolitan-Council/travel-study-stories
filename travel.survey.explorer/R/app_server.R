84b90dcaf2c2771df6cb1586245063054cf1fed8
Liz Roten <liz.roten@metc.state.mn.us>
2022-04-05 12:27:04 -0500

    create objects from variables, categories; pass to table and utils

--- travel.survey.explorer/R/app_server.R
+++ travel.survey.explorer/R/app_server.R
@@ -6,14 +6,18 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
-
+  # browser()
   # input category and variable-----
-  mod_input_category_variable_server("1w_input_1")
+  one_way_input <- mod_input_category_variable_server("1w_input_1")
+
   mod_input_category_variable_server("2w_input_1")
   mod_input_category_variable_server("2w_input_2")
 
 
+  one_way_tab <- mod_input_utils_server("input_utils_ui_1", one_way_input)
+
+
   # tables -----
-  mod_table_one_way_server("table_one_way_ui_1")
+  mod_table_one_way_server("table_one_way_ui_1", table_data = one_way_tab)
   mod_table_two_way_server("table_two_way_ui_1")
 }
