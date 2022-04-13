665364161942f545df9f3d82715a2edaca0687c1
Liz Roten <liz.roten@metc.state.mn.us>
2022-04-13 12:01:08 -0500

    add one way plot

--- travel.survey.explorer/R/app_server.R
+++ travel.survey.explorer/R/app_server.R
@@ -15,6 +15,7 @@ app_server <- function(input, output, session) {
 
 
   one_way_tab <- mod_input_utils_server("input_utils_ui_1", one_way_input)
+  mod_plot_one_way_server("plot_one_way_ui_1", one_way_tab)
 
 
   # tables -----
