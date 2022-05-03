4f97eed030b9d9d667ec7012cf579af7d66ba597
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-03 12:23:04 -0500

    clean up objects

--- travel.survey.explorer/data-raw/add-geographic-boundaries.R
+++ travel.survey.explorer/data-raw/add-geographic-boundaries.R
@@ -19,6 +19,7 @@ county_list <-
 city_list <- c("Minneapolis", "St. Paul")
 
 ### Create SF objects from TBI tables ------------
+message("Creating SF objects from TBI Tables")
 ##### households -----
 hh_sf <- hh %>%
   select(hh_id, home_lon, home_lat) %>%
@@ -59,9 +60,11 @@ school_sf <- per %>%
 
 
 ### Get Shapefiles -------------
-db <- DBI::dbConnect(odbc::odbc(), "GISLibrary")
+message("Downloading Shapefiles from GIS Library")
 
+db <- DBI::dbConnect(odbc::odbc(), "GISLibrary")
 ##### MPO: ----
+
 mpo_sf <- DBI::dbGetQuery(
   db,
   "SELECT *, SHAPE.STAsText() as geometry FROM GISLibrary.DBO.MetropolitanPlanningOrganizationArea;"
@@ -69,6 +72,7 @@ mpo_sf <- DBI::dbGetQuery(
   st_as_sf(wkt = "geometry", crs = 26915) %>%
   st_transform(crs = 4326) %>%
   st_make_valid()
+
 
 ##### Counties: ----
 # Minnesota:
@@ -95,7 +99,6 @@ wi_cty_sf <- DBI::dbGetQuery(db,
 cty_sf <- rbind(mn_cty_sf, wi_cty_sf)%>%
   st_make_valid()
 
-
 ##### Cities: ----
 ctu_sf <- DBI::dbGetQuery(db,
                           "SELECT *, SHAPE.STAsText() as geometry FROM GISLibrary.DBO.CTUs;") %>%
@@ -106,18 +109,61 @@ ctu_sf <- DBI::dbGetQuery(db,
   st_make_valid()
 
 ##### Thrive: ----
-thrive2040 <- dbGetQuery(
+thrive_sf <- dbGetQuery(
   db,
   "SELECT *, SHAPE.STAsText() as geometry FROM GISLibrary.DBO.THRIVEMSP2040COMMUNITYDESIGNATION;"
 ) %>%
-  sst_as_sf(wkt = "geometry", crs = 26915) %>%
+  st_as_sf(wkt = "geometry", crs = 26915) %>%
   select(COMDESNAME) %>%
   rename(thrive_category = COMDESNAME) %>%
   st_transform(crs = 4326) %>%
-  st_make_valid()
+  st_make_valid() %>%
+  mutate(thrive_category = factor(
+    thrive_category,
+    levels = c(
+      "Urban Center",
+      "Urban",
+      "Suburban",
+      "Suburban Edge",
+      "Emerging Suburban Edge",
+      "Rural Center",
+      "Rural Residential",
+      "Diversified Rural",
+      "Agricultural"
+    )
+  )) %>%
+  mutate(
+    thrive_category_broad = recode_factor(
+      thrive_category,
+      "Agricultural" = "Rural",
+      "Diversified Rural" = "Rural",
+      "Rural Center" = "Rural",
+      "Rural Residential" = "Rural"
+    )
+  ) %>%
+  mutate(thrive_category_broad = factor(
+    thrive_category_broad,
+    levels = c(
+      "Urban Center",
+      "Urban",
+      "Suburban",
+      "Suburban Edge",
+      "Emerging Suburban Edge",
+      "Rural"
+    )
+  )) %>%
+  mutate(
+    urban_rural_suburban = recode_factor(
+      thrive_category_broad,
+      "Urban Center" = "Urban",
+      "Suburban Edge" = "Suburban",
+      "Emerging Suburban Edge" = "Suburban"
+    )
+  )
 
 
 ### Household geographic Info ----------
+message("Appending geographic boundaries to household locations")
 ##### MPO: ----
 hh_mpo <-
   st_join(hh_sf, mpo_sf %>% select(OBJECTID), join = st_within) %>%
@@ -143,14 +189,45 @@ hh_ctu <-
   )) %>%
   select(-community_name)
 
+##### Thrive: ----
+hh_thrive <-
+  st_join(hh_sf, thrive_sf, join = st_within) %>%
+  st_drop_geometry()
+
+# additional information from urban/rural/suburban
+hh_thrive2 <-
+  hh %>%
+  select(hh_id, sample_segment) %>%
+  left_join(hh_thrive) %>%
+  mutate(
+    urban_rural_suburban =
+      case_when(
+        sample_segment %in% c("rural_ring", "core_rural") &
+          is.na(urban_rural_suburban) ~ "Rural",
+        sample_segment == "core_urban" &
+          is.na(urban_rural_suburban) ~ "Urban",
+        TRUE ~ as.character(urban_rural_suburban)
+      )
+  ) %>%
+  mutate(
+    urban_rural_suburban = factor(
+      urban_rural_suburban,
+      levels = c("Urban", "Suburban","Rural")
+    )
+  )
+
+#### Compile, write over hh table:
 hh <- hh %>%
   left_join(hh_mpo) %>%
   left_join(hh_cty) %>%
   left_join(hh_ctu) %>%
+  left_join(hh_thrive2) %>%
   mutate(across(c(hh_in_mpo, hh_county, hh_city), ~ as.factor(.)))
 
 ### Append Geographic Info to Trip Origin & Destination ----------
+message("Appending geographic information to trip origin & destination")
 ##### MPO: ----
+message("... trip origin/destination within MPO boundary...")
 trip_o_mpo <-
   st_join(trip_o_sf, mpo_sf %>% select(OBJECTID), join = st_within) %>%
   st_drop_geometry() %>%
@@ -164,6 +241,7 @@ trip_d_mpo <-
   mutate(trip_d_in_mpo = case_when(trip_d_in_mpo == 1 ~ "Trip ends in Twin Cities region", TRUE ~ "Trip ends outside Twin Cities region"))
 
 ##### County: ----
+message("... trip origin/destination county...")
 trip_o_cty <-
   st_join(trip_o_sf, cty_sf, join = st_within) %>%
   st_drop_geometry() %>%
@@ -181,6 +259,7 @@ trip_d_cty <-
   ))
 
 ##### City: ----
+message("... trip origin/destination city...")
 trip_o_ctu <-
   st_join(trip_o_sf, ctu_sf, join = st_within) %>%
   st_drop_geometry() %>%
@@ -197,19 +276,32 @@ trip_d_ctu <-
   )) %>%
   select(-community_name)
 
-trip_test <- trip %>%
+##### Thrive: -----
+message("... trip origin/destination thrive category...")
+
+trip_o_thrive <-
+  st_join(trip_o_sf, thrive_sf, join = st_within) %>%
+  st_drop_geometry()
+
+trip_d_thrive <-
+  st_join(trip_d_sf, thrive_sf, join = st_within) %>%
+  st_drop_geometry()
+
+##### Compile: -----
+trip <- trip %>%
   left_join(trip_o_mpo) %>%
   left_join(trip_d_mpo) %>%
   left_join(trip_o_cty) %>%
   left_join(trip_d_cty) %>%
   left_join(trip_o_ctu) %>%
   left_join(trip_d_ctu) %>%
+  left_join(trip_o_thrive) %>%
+  left_join(trip_d_thrive) %>%
   mutate(across(c(trip_o_in_mpo, trip_o_county, trip_o_city,
                   trip_d_in_mpo, trip_d_county, trip_d_city), ~ as.factor(.)))
 
-summary(trip_test)
-
 ### Append Geographic Info to Work ----
+message("Appending geographic information to work locations (person table)")
 ##### MPO: ----
 work_mpo <-
   st_join(work_sf, mpo_sf %>% select(OBJECTID), join = st_within) %>%
@@ -235,19 +327,22 @@ work_ctu <-
   )) %>%
   select(-community_name)
 
-hh <- hh %>%
-  left_join(work_mpo) %>%
-  left_join(work_cty) %>%
-  left_join(work_ctu) %>%
-  mutate(across(c(work_in_mpo, work_county, work_city), ~ as.factor(.)))
+##### Thrive: ----
+work_thrive <-
+  st_join(work_sf, thrive_sf, join = st_within) %>%
+  st_drop_geometry()
 
+#### Compile: -----
 per <- per %>%
+  select(-work_county) %>%
   left_join(work_mpo) %>%
   left_join(work_cty) %>%
   left_join(work_ctu) %>%
+  left_join(work_thrive) %>%
   mutate(across(c(work_in_mpo, work_county, work_city), ~ as.factor(.)))
 
 ### Append Geographic Info to School ----
+message("Appending geographic information to school locations (person table)")
 ##### MPO: ----
 school_mpo <-
   st_join(school_sf, mpo_sf %>% select(OBJECTID), join = st_within) %>%
@@ -273,9 +368,57 @@ school_ctu <-
   )) %>%
   select(-community_name)
 
+
+##### Thrive: ----
+school_thrive <-
+  st_join(school_sf, thrive_sf, join = st_within) %>%
+  st_drop_geometry()
+
+##### Compile: -----
 per <- per %>%
+  select(-school_county) %>%
   left_join(school_mpo) %>%
   left_join(school_cty) %>%
   left_join(school_ctu) %>%
+  left_join(school_thrive) %>%
   mutate(across(c(school_in_mpo, school_county, school_city), ~ as.factor(.)))
 
+# Clean up:
+rm(
+  "city_list",
+  "county_list",
+  "ctu_sf",
+  "cty_sf",
+  "hh_ctu",
+  "hh_cty",
+  "hh_mpo",
+  "hh_sf",
+  "hh_thrive",
+  "hh_thrive2",
+  "mn_cty_sf",
+  "mpo_sf",
+  "school_ctu",
+  "school_cty",
+  "school_mpo",
+  "school_sf",
+  "school_thrive",
+  "thrive_sf",
+  "trip_d_ctu",
+  "trip_d_cty",
+  "trip_d_mpo",
+  "trip_d_sf",
+  "trip_d_thrive",
+  "trip_o_ctu",
+  "trip_o_cty",
+  "trip_o_mpo",
+  "trip_o_sf",
+  "trip_o_thrive",
+  "wi_cty_sf",
+  "work_ctu",
+  "work_cty",
+  "work_mpo",
+  "work_sf",
+  "work_thrive"
+)
+
+rm(db)
