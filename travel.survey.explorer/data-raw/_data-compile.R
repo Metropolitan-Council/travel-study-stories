55cc5e107aa4dd4fe6a4d4434ed73ac39f0348e3
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-03 10:05:41 -0500

    remove PII as own script/step

--- travel.survey.explorer/data-raw/_data-compile.R
+++ travel.survey.explorer/data-raw/_data-compile.R
@@ -2,7 +2,7 @@
 source("data-raw/get-survey-data.R")
 
 # Trim survey data to MPO region -----------
-source("data-raw/add-mpo-boundary-to-hh.R")
+source("data-raw/add-geographic-boundaries.R")
 
 # Get EPA Efficiency Data -----------
 source("data-raw/get-epa-vehicle-efficiency-data.R")
@@ -25,32 +25,8 @@ trip <- trip %>%
   mutate(depart_time_imputed = as.ITime(depart_time_imputed),
          arrive_time = as.ITime(arrive_time))
 
-# check for any PII and remove
-# for vehicle table, remove make, model, year and name, and then round the numbers from DPS/EPA
-veh <- veh %>%
-  select(-make, -model, -vehicle_name, -class_vehicle) %>%
-  select(-epa_tbi_veh_match_notes, -dps_tbi_veh_match_notes) %>%
-  mutate(veh_age = 2019 - year) %>%
-  select(-year) %>%
-  mutate(
-    co2_gpm = round(co2_gpm, -1),
-    mpg_city = round(mpg_city, 0),
-    mpg_highway = round(mpg_highway, 0),
-    weight_unladen = round(weight_unladen, -2)
-  ) %>%
-  left_join(hh %>% select(hh_id, hh_weight))
-
-hh <-
-  hh %>%
-  select(-home_lat, -home_lon)
-
-trip <-
-  trip %>%
-  select(-o_lat, -o_lon, -d_lat, -d_lon)
-
-
-per <- per %>%
-  select(-ethnicity_other_specify)
+# Remove PII ------------------
+source("data-raw/remove-pii.R")
 
 # Trim columns down for manageability ----------
 # source("data-raw/slim-survey-data-columns.R")
