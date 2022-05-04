18ca35d1354fcd816a376b8d0cb58ff45204ba9a
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-04 16:09:41 -0500

    FULL! DICTIONARY! completed by hand and added to tbi_dict.rda

--- travel.survey.explorer/data-raw/_data-compile.R
+++ travel.survey.explorer/data-raw/_data-compile.R
@@ -11,7 +11,12 @@ source("data-raw/get-epa-vehicle-efficiency-data.R")
 source("data-raw/get-dps-vehicle-weight-data.R")
 
 # Extra variables ------
-
+source("data-raw/add-var-person-race.R")
+source("data-raw/add-var-hh-income-easyread.R")
+source("data-raw/add-var-trip-purpose.R")
+source("data-raw/add-var-trip-mode-group.R")
+source("data-raw/add-var-trip-purpose-broad.R")
+source("data-raw/add-var-trip-seasons.R")
 
 # Re-format time
 trip <- trip %>%
@@ -24,9 +29,6 @@ source("data-raw/remove-pii.R")
 # Trim columns down for manageability ----------
 # source("data-raw/slim-survey-data-columns.R")
 
-# Work on the dictionary ------------------
-source("data-raw/create-dictionary.R")
-
 
 # Write Data -------------------------
 tbi_tables <- list(
@@ -34,7 +36,8 @@ tbi_tables <- list(
   "per" = per,
   "hh" = hh,
   "veh" = veh,
-  "trip" = trip
+  "trip" = trip,
+  "trip_purpose" = trip_purpose
 )
 
 usethis::use_data(tbi_tables,
@@ -42,5 +45,10 @@ usethis::use_data(tbi_tables,
                   compress = "xz",
                   internal = FALSE
 )
+
+# Work on the dictionary ------------------
+source("data-raw/create-dictionary.R")
+
+
 
 
