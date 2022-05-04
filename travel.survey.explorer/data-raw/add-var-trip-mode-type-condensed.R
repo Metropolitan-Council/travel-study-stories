b5b9d087cb442a3273e8db6da0e4669d10bc8707
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-04 13:38:38 -0500

    minor changes to make compatible with tables here

--- travel.survey.explorer/data-raw/add-var-trip-mode-type-condensed.R
+++ travel.survey.explorer/data-raw/add-var-trip-mode-type-condensed.R
@@ -1,9 +1,9 @@
-tbi_tables$trip <-
-  tbi_tables$trip %>%
+trip <-
+  trip %>%
   mutate(mode_type = as.character(mode_type)) %>%
   mutate(mode_type = ifelse(grepl("bicy", mode_type_detailed), "Bicycle", mode_type)) %>%
   mutate(
-    mode_type_cond =
+    mode_group =
       recode_factor(mode_type,
         `Household vehicle` = "Drive",
         `School bus` = "Other",
@@ -20,9 +20,3 @@ tbi_tables$trip <-
         `Long distance passenger mode` = "Other"
       )
   )
-
-# Append to dictionary
-new_entry <- data.frame(which_table = "trip", variable = "mode_type_cond", wt_field = "trip_weight", category = "Trips", variable_label = "Mode Type Category",
-                        value = unique(tbi_tables$trip$mode_type_cond))
-tbi_dict <- bind_rows(tbi_dict, new_entry)
-message("New column created in trip table: mode_type_cond")
