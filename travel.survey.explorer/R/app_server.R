7a6477c889b8f0ee39c7cb3125fff27f55dd9889
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-06 21:37:55 -0500

    trying to get input utils to talk to server

--- travel.survey.explorer/R/app_server.R
+++ travel.survey.explorer/R/app_server.R
@@ -11,14 +11,18 @@ app_server <- function(input, output, session) {
 
   # inputId inside the server needs to match ui inputId
   one_way_input <- mod_input_category_variable_server("oneway_input_1")
+  one_way_data <- mod_filters_oneway_server()
+  one_way_tab <- mod_input_utils_server(
+    "input_utils_ui_1",
+    user_inputs = one_way_input,
+    user_data = one_way_data)
 
   mod_input_category_variable_server("twoway_input_1")
   mod_input_category_variable_server("twoway_input_2")
 
 
-  one_way_tab <- mod_input_utils_server(
-    "input_utils_ui_1",
-    user_inputs = one_way_input)
+
+
   mod_plot_one_way_server("plot_one_way_ui_1", one_way_inputs = one_way_tab)
 
 
