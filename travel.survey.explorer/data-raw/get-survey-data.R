c1c5ec23eba8d9f26620093acf1e0202f4b9937c
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-06 20:07:29 -0500

    add vehicle average stats to trips in another private vehicle

--- travel.survey.explorer/data-raw/get-survey-data.R
+++ travel.survey.explorer/data-raw/get-survey-data.R
@@ -154,11 +154,37 @@ veh <-
   veh %>% mutate(veh_id = paste(hh_id, "_", vehicle_num, sep = ""))
 
 trip <- trip %>%
+  mutate(veh_id = case_when(
+    grepl(pattern = "Household vehicle", x = mode_type_detailed) ~ mode_type_detailed
+  )) %>%
   mutate(veh_id = str_replace(
-    mode_type_detailed,
+    veh_id,
     pattern = "Household vehicle ",
     replacement = paste(hh_id, "_", sep = "")
-  ))
+  )) %>%
+  mutate(veh_id = case_when(mode_type_detailed %in%
+                              c("Other vehicle in household",
+                                "Other motorcycle",
+                                "Car from work",
+                                "Friend/relative/colleague's car",
+                                "Rental car",
+                                "Carpool match (e.g., Waze Carpool)",
+                                "Carshare service (e.g., HOURCAR, Car2Go, Zipcar, Maven)",
+                                "Peer-to-peer car rental (e.g., Turo, Getaround)",
+                                "Other vehicle") ~
+                              "Other Private Vehicle"))
+
+region_average_veh_stats <-
+  veh %>%
+  left_join(hh %>% select(hh_id, hh_weight)) %>%
+  filter(mpg_city < Inf, mpg_highway < Inf) %>%
+  summarize(across(where(is.numeric), ~weighted.mean(., na.rm = T, w = hh_weight))) %>%
+  select(-hh_id, -vehicle_num, -hh_weight) %>%
+  mutate(veh_id = "Other Private Vehicle")
+
+trip %>%
+  left_join(region_average_veh_stats) %>%
+  filter(veh_id == "Other Private Vehicle")
 
 ## Clean up---------------
 rm(connect_string, replace_survey_missing, translate_using_dictionary, tbidb)
