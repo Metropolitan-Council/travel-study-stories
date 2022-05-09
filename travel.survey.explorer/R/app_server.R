0948d32588d9bec31405c1b81d5de09e2e092065
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-05-08 19:42:56 -0500

    rearrange; add headers & sub-headers

--- travel.survey.explorer/R/app_server.R
+++ travel.survey.explorer/R/app_server.R
@@ -7,29 +7,42 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   # browser()
-  # input category and variable-----
 
-  # inputId inside the server needs to match ui inputId
+  # One-way tab -------
+
+  ## input category and variable-----
   one_way_input <- mod_input_category_variable_server("oneway_input_1")
+
+  ## list of hh ids-----
   one_way_hhs <- mod_filters_oneway_server("filters_oneway_1")
+
+  ## raw table data------
   one_way_tab <- mod_input_utils_server(
     "input_utils_ui_1",
     user_inputs = one_way_input,
     user_hhs = one_way_hhs)
 
+  ## formatted table -----
+  mod_table_one_way_server("table_one_way_ui_1",
+                           one_way_inputs = one_way_tab
+  )
+
+  ## plot--------
+  mod_plot_one_way_server("plot_one_way_ui_1", one_way_inputs = one_way_tab)
+
+  # Two-way tab -------
+
+  ## input category and variable-----
   mod_input_category_variable_server("twoway_input_1")
   mod_input_category_variable_server("twoway_input_2")
 
+  ## list of hh ids-----
 
+  ## raw table data------
 
-
-  mod_plot_one_way_server("plot_one_way_ui_1", one_way_inputs = one_way_tab)
-
-
-  # tables -----
-  mod_table_one_way_server("table_one_way_ui_1",
-    one_way_inputs = one_way_tab
-  )
-
+  ## formatted table -----
   mod_table_two_way_server("table_two_way_ui_1")
+
+  ## plot--------
+
 }
