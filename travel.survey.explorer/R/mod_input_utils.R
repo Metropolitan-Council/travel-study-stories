7f29b964fe982268f0028c7c1084dd6d39d22c59
Liz Roten <liz.roten@metc.state.mn.us>
2022-06-07 16:24:46 -0500

    remove separate two_way input processing
    slow start-up slightly, but same processing speed otherwise

--- travel.survey.explorer/R/mod_input_utils.R
+++ travel.survey.explorer/R/mod_input_utils.R
@@ -18,35 +18,78 @@ mod_input_utils_ui <- function(id) {
 #'
 #' @return [shiny::reactiveValues()] `table_data` and `context_data`.
 #'
-mod_input_utils_server <- function(id, user_var, user_hhs) {
+mod_input_utils_server <- function(id,
+                                   user_var,
+                                   util_hh_ids,
+                                   n_ways = 1,
+                                   util_variable_row,
+                                   util_variable_col) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
     vals <- reactiveValues()
 
     # generate table
-    one_way_rt_list <- reactive({
-      create_one_way_table(
-        variable_row = user_var$variable,
-        hh_ids = user_hhs$hh_ids
-      )
+
+    rt_list <- reactive({
+      if(n_ways == 1){
+
+        t <- create_one_way_table(
+          variable_row = user_var$variable,
+          hh_ids = util_hh_ids$hh_ids
+        )
+        return(t)
+
+      } else {
+
+        t <- create_two_way_table(
+          variable_row = util_variable_row$variable,
+          variable_col = util_variable_col$variable,
+          hh_ids = util_hh_ids$hh_ids
+        )
+        return(t)
+
+      }
     })
 
-    observe({
-      # browser()
-      one_way_rt <- one_way_rt_list()
 
-      vals$table_return <- one_way_rt$table
-      vals$definitions_return <- one_way_rt$definitions
-      vals$summary_return <- one_way_rt$summary
+    # browser()
+    if(n_ways == 1){
+      observe({
+        one_way_rt <- rt_list()
 
-      print(vals$table_return) # eventually comment out
+        vals$table_return <- one_way_rt$table
+        vals$definitions_return <- one_way_rt$definitions
+        vals$summary_return <- one_way_rt$summary
 
-      return(vals)
-    }) %>%
-      bindEvent(user_var$variable, user_hhs$hh_ids)
+        print(vals$table_return) # eventually comment out
+
+        return(vals)
+      }) %>%
+        bindEvent(user_var$variable, util_hh_ids$hh_ids)
+
+    } else {
+
+      observe({
+        two_way_rt <- rt_list()
+
+        vals$two_way_list <- two_way_rt
+        vals$table_return <- two_way_rt$table
+        vals$context_row_return <- two_way_rt$definition_row
+        vals$context_col_return <- two_way_rt$definition_col
+
+        vals$summary_return <- two_way_rt$summary
+
+        print(vals$table_return) # eventually comment out
+
+        return(vals)
+      }) %>%
+        bindEvent(util_variable_row$variable, util_variable_col$variable, util_hh_ids$hh_ids)
+
+    }
 
     return(vals)
+
   })
 }
 
