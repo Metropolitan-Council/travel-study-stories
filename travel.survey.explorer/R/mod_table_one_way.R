1cff7b0587b40be4e2a4c4601f48f311300e0316
Liz Roten <liz.roten@metc.state.mn.us>
2022-06-13 14:45:09 -0500

    gray out instances with group N under 30
    gray out entire row

--- travel.survey.explorer/R/mod_table_one_way.R
+++ travel.survey.explorer/R/mod_table_one_way.R
@@ -21,42 +21,46 @@ mod_table_one_way_server <- function(id, one_way_table_inputs) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     w <- waiter::Waiter$new(ns("table"),
-      color = waiter::transparent(0.5)
+                            color = waiter::transparent(0.5)
     )
 
 
 
     output$table <- DT::renderDataTable({
       w$show()
-
       definitions_data <- one_way_table_inputs$definitions_return
       summary_data <- one_way_table_inputs$summary_return
       table_data <- one_way_table_inputs$table_return
 
       DT::datatable(table_data %>%
-        dplyr::select(
-          1,
-          estimated_prop,
-          estimated_prop_se,
-          group_N
-        ) %>%
-        mutate(estimated_prop_se = scales::percent(estimated_prop_se,
-          accuracy = 0.01
-        )),
-      rownames = F,
-      colnames = c(
-        definitions_data$variable_label,
-        "Estimated proportion",
-        "Standard error",
-        "Group size"
-      )
+                      dplyr::select(
+                        1,
+                        estimated_prop,
+                        estimated_prop_se,
+                        group_N
+                      ) %>%
+                      mutate(estimated_prop_se = scales::percent(estimated_prop_se,
+                                                                 accuracy = 0.01
+                      )),
+                    rownames = F,
+                    colnames = c(
+                      definitions_data$variable_label,
+                      "Estimated proportion",
+                      "Standard error",
+                      "Group size"
+                    )
       ) %>%
         DT::formatPercentage(columns = c(2), digits = 2) %>%
         DT::formatString(
           columns = c(3),
           prefix = "+/-"
         ) %>%
-        DT::formatRound(columns = c(4), digits = 0)
+        DT::formatRound(columns = c(4), digits = 0) %>%
+        DT::formatStyle(
+          valueColumns = "group_N",
+          color = DT::styleInterval(30, c("#bdbdc3", councilR::colors$suppBlack)),
+          columns = 1:4
+        )
     })
   })
 }
