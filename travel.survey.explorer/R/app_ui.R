b704fbecc61056ea581477adacbd9d103c5ad6cd
Liz Roten <liz.roten@metc.state.mn.us>
2022-06-15 16:04:53 -0500

    set starting category choices to restrict column

--- travel.survey.explorer/R/app_ui.R
+++ travel.survey.explorer/R/app_ui.R
@@ -45,11 +45,13 @@ app_ui <- function(request) {
                  p(shinipsum::random_text(nwords = 50))),
           shiny::column(width = 3,
                         h4("First variable (rows)"),
-                        mod_input_category_variable_ui("input_category_row_1")),
+                        mod_input_category_variable_ui("input_category_row_1",
+                                                       starting_category_choices = "Demographics")),
           column(width = 3,
                  h4("Second variable (columns)"),
                  mod_input_category_variable_ui(
                    "input_category_col_1",
+                   starting_category_choices = unique(tbi_dict$category)[-1],
                    starting_category_selected = "Shared mobility",
                    starting_variable_choices = input_list$`Shared mobility`,
                    starting_variable_selected = "How often uses a bicycle"
