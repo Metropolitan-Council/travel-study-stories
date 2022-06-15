998bd0e5eb9dc73606e7c485ffe71afa9cc079e9
Liz Roten <liz.roten@metc.state.mn.us>
2022-06-15 16:00:29 -0500

    remove some input choices for app only

--- travel.survey.explorer/data-raw/input_list.R
+++ travel.survey.explorer/data-raw/input_list.R
@@ -3,8 +3,11 @@
 library(dplyr)
 # library(travel.survey.explorer)
 
-input_list <- purrr::map(unique(tbi_dict$category), function(x) {
-  tb <- tbi_dict %>%
+tbi_dict_app <- tbi_dict %>%
+  filter(stringr::str_detect(variable, "ethnicity", negate = T))
+
+input_list <- purrr::map(unique(tbi_dict_app$category), function(x) {
+  tb <- tbi_dict_app %>%
     dplyr::filter(category == x)
 
   variab <- tb$variable %>%
@@ -19,19 +22,17 @@ input_list <- purrr::map(unique(tbi_dict$category), function(x) {
   return(variab)
 })
 
-names(input_list) <- unique(tbi_dict$category)
+names(input_list) <- unique(tbi_dict_app$category)
 
 
 usethis::use_data(input_list, overwrite = TRUE)
 
 
-## code to prepare `input_list` dataset goes here
+## code to prepare `input_question_list` dataset goes here
 
-library(dplyr)
-# library(travel.survey.explorer)
 
-input_question_list <- purrr::map(unique(tbi_dict$variable), function(x) {
-  tb <- tbi_dict %>%
+input_question_list <- purrr::map(unique(tbi_dict_app$variable), function(x) {
+  tb <- tbi_dict_app %>%
     dplyr::filter(variable == x)
 
   variab <- tb$variable %>%
@@ -48,7 +49,7 @@ input_question_list <- purrr::map(unique(tbi_dict$variable), function(x) {
 })
 
 
-names(input_question_list) <- unique(tbi_dict$variable)
+names(input_question_list) <- unique(tbi_dict_app$variable)
 
 
 usethis::use_data(input_question_list, overwrite = TRUE)
