d9dd25354acd119c21dd5a2af5c416d1ce9428c8
Liz Roten <liz.roten@metc.state.mn.us>
2022-06-21 14:23:29 -0500

    change "about" to "markdown"; use dynamic id to pull which md

--- travel.survey.explorer/R/mod_markdown.R
+++ travel.survey.explorer/R/mod_markdown.R
@@ -1,4 +1,4 @@
-#' about UI Function
+#' @title Markdown module UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -7,28 +7,29 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-mod_about_ui <- function(id) {
+mod_markdown_ui <- function(id) {
   ns <- NS(id)
 
   shiny::div(
     id = "notes",
-    includeMarkdown(system.file("app/www/ABOUT.md",
-      package = "travel.survey.explorer"
-    ))
+    includeMarkdown(system.file(paste0("app/www/",id,".md"),
+                                package = "travel.survey.explorer"
+    )),
+    reference_table()
   )
 }
 
 #' @title about Server Functions
 #'
 #' @noRd
-mod_about_server <- function(id) {
+mod_markdown_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
   })
 }
 
 ## To be copied in the UI
-# mod_about_ui("about_ui_1")
+# mod_markdown_ui("about_ui_1")
 
 ## To be copied in the server
 # mod_about_server("about_ui_1")
