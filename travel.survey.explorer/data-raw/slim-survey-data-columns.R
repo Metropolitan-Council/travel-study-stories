d1ad80641fa62650d7aceeab1afec7377dffc2f3
Liz Roten <liz.roten@metc.state.mn.us>
2022-07-11 13:33:00 -0500

    only select columns that exist

--- travel.survey.explorer/data-raw/slim-survey-data-columns.R
+++ travel.survey.explorer/data-raw/slim-survey-data-columns.R
@@ -6,8 +6,6 @@ trip <- trip %>%
     travel_date, depart_time_imputed, arrive_time,
     duration_imputed, distance, speed_mph_imputed,
     leg_num, linked_trip_num,
-    o_lat, o_lon,
-    d_lat, d_lon,
     num_travelers,
     d_purpose_category_imputed, d_purpose_imputed,
     o_purpose_category_imputed, o_purpose_imputed,
@@ -24,7 +22,6 @@ day <- day %>%
 hh <- hh %>%
   select(
     hh_id, hh_weight,
-    home_lon, home_lat,
     residence_type, rent_own, rent_cost,
     num_adults, num_kids, num_students, num_workers, num_people, num_vehicles,
     income_broad, income_detailed
@@ -38,5 +35,7 @@ per <- per %>%
     job_type, license, num_jobs
   )
 
-veh <- veh %>%
-  select(hh_id, vehicle_num, vehicle_name, year, make, model, fuel, veh_id)
+# veh <- veh %>%
+#   select(hh_id, vehicle_num,
+#          # vehicle_name,
+#          year, make, model, fuel, veh_id)
