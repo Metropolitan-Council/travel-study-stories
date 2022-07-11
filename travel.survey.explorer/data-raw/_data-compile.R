cfbb5d4c69e13c63b0652d9d51e27fa9832d6d9b
Liz Roten <liz.roten@metc.state.mn.us>
2022-07-11 13:43:38 -0500

    configure tbi_tables

--- travel.survey.explorer/data-raw/_data-compile.R
+++ travel.survey.explorer/data-raw/_data-compile.R
@@ -1,22 +1,33 @@
 # Get TBI data from helper -----
 pkgload::load_all()
+library(data.table)
 load("../../metc.tbi.helper/data/tbi19.rda")
+
+
+day <- tbi19$day
+per <- tbi19$per
+hh <- tbi19$hh
+veh <- tbi19$veh
+trip <- tbi19$trip
+trip_purpose <- tbi19$trip_purpose
+
+
 source("data-raw/slim-survey-data-columns.R")
 
 # Write Data -------------------------
 tbi_tables <- list(
-  "day" = tbi19$day,
-  "per" = tbi19$per,
-  "hh" = tbi19$hh,
-  "veh" = tbi19$veh,
-  "trip" = tbi19$trip,
-  "trip_purpose" = tbi19$trip_purpose
+  "day" = day,
+  "per" = per,
+  "hh" = hh,
+  "veh" = veh,
+  "trip" = trip,
+  "trip_purpose" = trip_purpose
 )
 
 usethis::use_data(tbi_tables,
-  overwrite = TRUE,
-  compress = "xz",
-  internal = FALSE
+                  overwrite = TRUE,
+                  compress = "xz",
+                  internal = FALSE
 )
 
 # Work on the dictionary ------------------
