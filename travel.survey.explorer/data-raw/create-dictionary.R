9becf8b6cae16fcef206b93aa6a52d08e982a16e
Liz Roten <liz.roten@metc.state.mn.us>
2022-08-04 17:20:55 -0500

    remove ASCII in dictionary

--- travel.survey.explorer/data-raw/create-dictionary.R
+++ travel.survey.explorer/data-raw/create-dictionary.R
@@ -1,3 +1,4 @@
+pkgload::load_all()
 
 # some work by hand occurred:
 
@@ -5,7 +6,9 @@ tbi_dict <- read.csv("data-raw/full_dictionary_filled.csv")
 # tbi_dict <- tbi19$dictionary
 
 tbi_dict <- tbi_dict %>%
-  filter(!category %in% c("Survey metadata", "PII"))
+  filter(!category %in% c("Survey metadata", "PII")) %>%
+  mutate(dplyr::across(everything(),
+                       iconv, "latin1", "ASCII", sub=""))
 
 
 usethis::use_data(tbi_dict,
