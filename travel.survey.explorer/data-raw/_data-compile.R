c71284dcdd3c56766cba92db4ca96673986b32e0
Liz Roten <liz.roten@metc.state.mn.us>
2022-08-04 17:51:17 -0500

    run all with tbi19, removing PII

--- travel.survey.explorer/data-raw/_data-compile.R
+++ travel.survey.explorer/data-raw/_data-compile.R
@@ -4,31 +4,31 @@ library(data.table)
 library(bit64)
 load("../../metc.tbi.helper/data/tbi19.rda")
 
-
-day <- tbi19$day
-per <- tbi19$per
-hh <- tbi19$hh
-veh <- tbi19$veh
-trip <- tbi19$trip
-trip_purpose <- tbi19$trip_purpose
-dictionary <- tbi19$dictionary
-
- # source("data-raw/slim-survey-data-columns.R")
+# source("data-raw/slim-survey-data-columns.R")
+source("data-raw/missing_pii_codes.R")
 
 # Write Data -------------------------
 tbi_tables <- list(
-  "day" = day,
-  "per" = per,
-  "hh" = hh,
-  "veh" = veh,
-  "trip" = trip,
-  "trip_purpose" = trip_purpose
-)
+  "day" = tbi19$day,
+  "per" = tbi19$per,
+  "hh" = tbi19$hh,
+  "veh" = tbi19$veh,
+  "trip" = tbi19$trip,
+  "trip_purpose" = tbi19$trip_purpose
+) |>
+  purrr::map(function(x) {
+    x |>
+      mutate(across(contains("_id"), bit64::as.integer64)) |>
+      select(-any_of(pii_codes))
+  })
+
+# waldo::compare(travel.survey.explorer::tbi_tables, tbi_tables)
+rm(tbi19)
 
 usethis::use_data(tbi_tables,
-                  overwrite = TRUE,
-                  compress = "xz",
-                  internal = FALSE
+  overwrite = TRUE,
+  compress = "xz",
+  internal = FALSE
 )
 
 # Work on the dictionary ------------------
@@ -37,4 +37,3 @@ source("data-raw/create-dictionary.R")
 # Create additional outputs for app --------------
 source("data-raw/histogram_breaks.R")
 source("data-raw/input_list.R")
-source("data-raw/missing_codes.R")
