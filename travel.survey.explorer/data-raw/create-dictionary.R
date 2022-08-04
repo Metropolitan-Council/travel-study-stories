5e3ed6f8d01bc14d596fe9e2c456cc0f1455535c
Liz Roten <liz.roten@metc.state.mn.us>
2022-08-04 17:50:21 -0500

    use metc.tbi.helper dictionary

--- travel.survey.explorer/data-raw/create-dictionary.R
+++ travel.survey.explorer/data-raw/create-dictionary.R
@@ -1,20 +1,21 @@
 pkgload::load_all()
-
 # some work by hand occurred:
 
-tbi_dict <- read.csv("data-raw/full_dictionary_filled.csv")
-# tbi_dict <- tbi19$dictionary
+# tbi_dict <- read.csv("data-raw/full_dictionary_filled.csv")
+tbi_dict <- read.csv("../../metc.tbi.helper/data-raw/final_dictionary_2019.csv")
+# some work by hand occurred:
 
 tbi_dict <- tbi_dict %>%
   filter(!category %in% c("Survey metadata", "PII")) %>%
   mutate(dplyr::across(everything(),
-                       iconv, "latin1", "ASCII", sub=""))
+    iconv, "latin1", "ASCII",
+    sub = ""
+  ))
 
+waldo::compare(travel.survey.explorer::tbi_dict, tbi_dict)
 
 usethis::use_data(tbi_dict,
   overwrite = TRUE,
   compress = "xz",
   internal = FALSE
 )
-
-## Clean up---------------
